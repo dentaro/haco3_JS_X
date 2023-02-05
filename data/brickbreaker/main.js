359ad5f5a6f2f38ad52c74576bb53e5758e992df
dentaro <77978725+dentaro@users.noreply.github.com>
2023-02-05 19:25:29 +0900

    Add files via upload

--- data/brickbreaker/main.js
+++ data/brickbreaker/main.js
@@ -2,101 +2,96 @@
 px = 64
 py = 112
 pstep =3
-
 tx = 0
 ty = 0
 tvx =1
 tvy =1
+modetitle = 0
+modegame = 1
+modeover = 2
+mode = modetitle
 
-cw = 16
-ch = 8
-
-chrs = []
-
-new_chr = function(){
-    var chr = {};
-    chr = { x:0, y:0, touchflag:0};
-    chrs[chrs.length] = chr;
-  return chr
-}
-
+oA=[[24,8,0],[36,16,0],[48,8,0],[56,16,0]];
 setup = function(){
     color(1)
     fillrect(0,0,128,128)
-    
-    for(cx =0; cx<cw; cx++){
-        for(cy =0; cy<ch; cy++){
-        chrs[chrs.length] = new_chr()
-        }
-    }
 }
 
 
+//loop内はモードの切り替え
 loop = function(){
+    if (mode == modetitle)title()
+    else if(mode == modegame)game()
+    else if(mode == modeover)over()
+}
 
+//タイトル画面
+title = function(){
+  color(225,200,54)
+  fillrect(0,0,128,128)
+  color(44,90,0)
+  text("「レンガを破壊せよ！」", 27,42)
+  text("[ｖ]ホールドでグッと前に進むよ",6, 85)
+  tvx = Math.random()*2
+  tvy = Math.random()*2
+  if(btn(4)==true){
+      mode = modegame;
+  }
+}
+
+//ゲームオーバー画面
+over = function(){
+    color(7)
+    text("クリア！",46,48);
+    if(btn(4)==true){
+        mode = modetitle;
+        
+        for(i =0;i<oA.length;i++){
+        oA[i][2] = 0;
+        
+        }
+    }
+}
+//ゲーム画面
+game = function(){
     color(1)
     fillrect(0,0,128,128)
-    
     if(px>=0){
-    if(btn(0)>0){px -= pstep;}//>
+    if(btn(3)>0){px -= pstep;}//>
     }else{
     px=0
     }
-    
     if(px<=104){
-    if(btn(1)>0){px += pstep;}//<
+    if(btn(4)>0){px += pstep;}//<
     }else{
     px=104
     }
-    
-    
     for(i =0; i<3; i++){
-        spr(px + 8*i, py, 8, 8, 8*8, 8*14)//パドル
+        spr(px + 8*i, py, 8, 8, 8*4, 8*2)//パドル
     }
-    
-    //跳ね返った時に少しランダムにさせる
-    tvx += (Math.random()-0.5)/40
-    tvy += (Math.random()-0.5)/40
-    
-    
     if(tx > 128||tx < 0)tvx*=-1
-    
     if(tx > px && tx < px+8*3){//パドルに当たったら
         if(ty > py||ty < 0)tvy*=-1//跳ね返す
     }else{
         if(ty > 128||ty < 0)tvy*=-1//壁で跳ね返る
     }
-    
     tx = tx + tvx
     ty = ty + tvy
-    
-    spr(tx, ty, 8, 8, 8*6, 8*13)//たま
-    
-    var ccnt = 0
-    var xcnt = 0
-    var ycnt = 0
-    var getnum = 0
-    for (key in chrs)//この表記で連想配列内のキーにアクセスできるようになる
-    {
-        chrs[ccnt].x = xcnt%16 
-        chrs[ccnt].y = ycnt 
-        
-        // if(tx > (chrs[ccnt].x*8) && tx < (chrs[ccnt].x*8)+8 && ty > chrs[ccnt].y*8 && ty < chrs[ccnt].y*8)//当たったか判定
-        if(tx < chrs[ccnt].x*8 +4 && tx > chrs[ccnt].x*8 - 4 && ty < chrs[ccnt].y*8 +4 && ty > chrs[ccnt].y*8 - 4)//当たったか判定
-        {chrs[ccnt].touchflag = 1}
-        
-        if(chrs[ccnt].touchflag==0)//当たっていなければ描画
-        spr(chrs[ccnt].x*8, chrs[ccnt].y*8, 8, 8, 8*9, 8*0)//コイン
-        
-        if(chrs[ccnt].touchflag == 1)getnum++
-        
-        xcnt++
-        if(xcnt%16 == 0)ycnt++
-        ccnt++
+    spr(tx, ty, 8, 8, 8*6, 8*5);
+    getnum = 0
+    for(i =0;i<4;i++){//ブロックの数
+        if(tx < oA[i][0] + 8 && tx > oA[i][0] - 8 && ty < oA[i][1] +8 && ty > oA[i][1] - 8)//当たったか判定
+        {
+            oA[i][2] = 1
+        }
+        if(oA[i][2]==0){
+            spr(oA[i][0], oA[i][1], 8, 8, 8*0, 8*6)
+            getnum += 1
+        }
+
     }
-    
-    color(255,255,255);
-    text(String(cw*ch - getnum), 4, 120);
-    
-    
+    if(getnum==0){
+        
+        mode = modeover;
+    }
 }
