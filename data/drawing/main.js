9414cbcdc30b57bce0850e7162b12e2481281136
dentaro <77978725+dentaro@users.noreply.github.com>
2023-05-03 14:55:09 +0900

    Add files via upload

--- data/drawing/main.js
+++ data/drawing/main.js
@@ -11,7 +11,7 @@ loop = function(){
     tbtnid = tbtns( 0,  0,  120, 120,  row, col, true);//タッチボタン生成（一回）、表示、ID取得まで一括して行う。trueによりボタンIDを連続取得するタッチビットに
     if(pretbtnid - tbtnid != 0 && tbtnid != -1 ){//同じボタンを押していない　かつ //-1（ボタンが押されていない）状態ではない
         for(j=0; j<col; j++){
-            for(i=0; i<row; i++){nowBt = j*col+i+5
+            for(i=0; i<row; i++){nowBt = j*col+i+9
                 if(nowBt == tbtnid)rwtb(tbtnid,1)//押されたボタンを第２引数で　0状態だけ取得　1トグル　2鉛筆 3消しゴム にする
                 if(rwtb(nowBt)==true)color(3)//緑//押されたボタンのbool値をトグルせずにそのまま取得する
                 if(rwtb(nowBt)==false)color(10)//黄色//押されたボタンのbool値をトグルせずにそのまま取得する
